d50c76047b710c0bc41a7f31e467dc6beb6d6340
Pavel <pavelnix@yandex.ru>
2015-12-01 05:02:39 -0800

    empty project 2

---
+++ main/java/ru/pavelnix/SquareTriangle.java
@@ -0,0 +1,5 @@
+package ru.pavelnix;
+
+public class SquareTriangle {
+
+}
---
+++ test/java/ru/pavelnix/SquareTriangleTest.java
@@ -0,0 +1,13 @@
+package ru.pavelnix;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class SquareTriangleTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
