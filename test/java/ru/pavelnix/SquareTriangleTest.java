1293397b3efbd8f765582d6271faadfa51834bd7
Pavel <pavelnix@yandex.ru>
2015-12-01 05:26:49 -0800

    Test Driven Development 1

--- test/java/ru/pavelnix/SquareTriangleTest.java
+++ test/java/ru/pavelnix/SquareTriangleTest.java
@@ -1,13 +1,24 @@
 package ru.pavelnix;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SquareTriangleTest {
 
+    private Point vertexA;
+    private Point vertexB;
+    private Point vertexC;
+    private SquareTriangle squareTriangle;
+
     @Before
     public void setUp() throws Exception {
+        vertexA = new Point(0.0f,0.0f);
+    }
+
+    @Test
+    public void testSquareTriangle(){
 
     }
 }
