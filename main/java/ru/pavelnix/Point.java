ce4b22ecce0da8873b36935725c0826b8aa459b1
Pavel <pavelnix@yandex.ru>
2015-12-01 05:28:59 -0800

    Test Driven Development 2

--- main/java/ru/pavelnix/Point.java
+++ main/java/ru/pavelnix/Point.java
@@ -1,5 +1,27 @@
 package ru.pavelnix;
 
 public class Point {
+    private float coordinateX;
+    private float coordinateY;
 
+    public Point(float coordinateX, float coordinateY) {
+        setCoordinateX(coordinateX);
+        setCoordinateY(coordinateX);
+    }
+
+    public float getCoordinateX() {
+        return coordinateX;
+    }
+
+    public void setCoordinateX(float coordinateX) {
+        this.coordinateX = coordinateX;
+    }
+
+    public float getCoordinateY() {
+        return coordinateY;
+    }
+
+    public void setCoordinateY(float coordinateY) {
+        this.coordinateY = coordinateY;
+    }
 }
